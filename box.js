844d68e8befd2da205562b94dae39977ce028bad
ast*rsk <ast_rsk@debian>
2021-08-20 19:43:57 -0700

    maggie mode

--- box.js
+++ box.js
@@ -1,97 +1,102 @@
 class Box {
-    constructor(x, y, size) {
-        this.pos = createVector(x, y);
-        this.size = size || 40;
-        this.c = this.getNewColor();
-        this.lastC;
-        this.curC;
+  constructor(x, y, size, img) {
+    this.pos = createVector(x, y);
+    this.size = size || 40;
+    this.c = this.getNewColor();
+    this.lastC;
+    this.curC;
+    this.img = img;
+    this.baseSpeed = 1;
+    this.maxSpeed = 1.1;
+    this.minSpeed = -0.9;
+    this.vel = createVector(
+      random(-this.maxSpeed, this.maxSpeed),
+      random(-this.maxSpeed, this.maxSpeed)
+    );
+  }
 
-        this.baseSpeed = 1;
-        this.maxSpeed = 1.10;
-        this.minSpeed = -0.90;
-        this.vel = createVector(random(-this.maxSpeed, this.maxSpeed), 
-                                random(-this.maxSpeed, this.maxSpeed));
+  move() {
+    if (this.vel.x < this.minSpeed) {
+      this.vel.x = this.minSpeed;
+    }
+    if (this.vel.y < this.minSpeed) {
+      this.vel.y = this.minSpeed;
+    }
+    this.pos = p5.Vector.add(this.pos, this.vel);
+  }
+
+  hit() {
+    if (this.pos.x - this.size < 0 || this.pos.x + this.size > width) {
+      this.vel.x *= -1;
+      this.vel.x += random(-0.1, 0.1);
+      this.pos.x =
+        this.pos.x <= 0 + this.size ? this.size + 1 : width - this.size - 1;
+      this.colorSwitch();
+    }
+    if (this.pos.y - this.size < 0 || this.pos.y + this.size > height) {
+      this.vel.y *= -1;
+      this.vel.y += random(-0.1, 0.1);
+      this.pos.y =
+        this.pos.y - this.size < 0 ? this.size + 1 : height - this.size - 1;
+      this.colorSwitch();
+    }
+  }
+
+  update() {
+    this.hit();
+    this.move();
+  }
+
+  colorSwitch() {
+    this.lastC = this.curC;
+    let newC = this.getNewColor();
+    while (this.lastC === this.curC) {
+      newC = this.getNewColor();
+    }
+    this.c = newC;
+  }
+
+  getNewColor() {
+    let newColor;
+    switch (floor(random(0, 6))) {
+      // ROYGBIV
+      case 0:
+        this.curC = 0;
+        newColor = color(255, 0, 0);
+        break;
+      case 1:
+        this.curC = 1;
+        newColor = color(255, 127, 0);
+        break;
+      case 2:
+        this.curC = 2;
+        newColor = color(255, 255, 0);
+        break;
+      case 3:
+        this.curC = 3;
+        newColor = color(0, 255, 0);
+        break;
+      case 4:
+        this.curC = 4;
+        newColor = color(0, 0, 255);
+        break;
+      case 5:
+        this.curC = 5;
+        newColor = color(75, 0, 130);
+        break;
+      default:
+        this.curC = 6;
+        newColor = color(139, 0, 255);
+        break;
     }
 
-    move() {
-        if (this.vel.x < this.minSpeed) {
-            this.vel.x = this.minSpeed;
-        }
-        if (this.vel.y < this.minSpeed) {
-            this.vel.y = this.minSpeed;
-        }
-        this.pos = p5.Vector.add(this.pos, this.vel);
-    }
+    return newColor;
+  }
 
-    hit() {
-        if (this.pos.x - this.size < 0 || this.pos.x + this.size > width) {
-            this.vel.x *= -1;
-            this.vel.x += random(-0.1, 0.1);
-            this.pos.x = (this.pos.x <= 0 + this.size) ? this.size + 1 : width - this.size - 1;
-            this.colorSwitch();
-        }
-        if (this.pos.y - this.size < 0 || this.pos.y + this.size > height) {
-            this.vel.y *= -1;
-            this.vel.y += random(-0.1, 0.1);
-            this.pos.y = (this.pos.y - this.size < 0) ? this.size + 1 : height - this.size - 1;
-            this.colorSwitch();
-        }
-    }
-
-    update() {
-        this.hit();
-        this.move();
-    }
-
-    colorSwitch() {
-        this.lastC = this.curC
-        let newC = this.getNewColor();
-        while (this.lastC === this.curC) {
-            newC = this.getNewColor();
-        }
-        this.c = newC;
-    }
-
-    getNewColor() {
-        let newColor;
-        switch (floor(random(0, 6))) {
-            // ROYGBIV
-            case 0:
-                this.curC = 0;
-                newColor = color(255, 0, 0);
-                break;
-            case 1:
-                this.curC = 1;
-                newColor = color(255, 127, 0);
-                break;
-            case 2:
-                this.curC = 2;
-                newColor = color(255, 255, 0);
-                break;
-            case 3:
-                this.curC = 3;
-                newColor = color(0, 255, 0);
-                break;
-            case 4:
-                this.curC = 4;
-                newColor = color(0, 0, 255);
-                break;
-            case 5:
-                this.curC = 5;
-                newColor = color(75, 0, 130);
-                break;
-            default:
-                this.curC = 6;
-                newColor = color(139, 0, 255);
-                break;
-        }
-
-        return newColor;
-    }
-
-    show() {
-        fill(this.c);
-        noStroke();
-        rect(this.pos.x, this.pos.y, this.size * 2, this.size * 2);
-    }
+  show() {
+    //fill(this.c);
+    //noStroke();
+    //rect(this.pos.x, this.pos.y, this.size * 2, this.size * 2);
+    image(img, this.pos.x, this.pos.y, this.size * 2, this.size * 2);
+  }
 }
--- sketch.js
+++ sketch.js
@@ -3,51 +3,56 @@ let box = [];
 let clicks = 0;
 let slide;
 let slideVal;
+let img;
+
+function preload() {
+  img = loadImage("maggie_dvd.png");
+}
 
 function setup() {
-	cnv = createCanvas(800, 800);
-	rectMode(CENTER);
-	cnv.mouseClicked(makeBox);
-	createP('');
-	slide = createSlider(1, 50, 40, 1);
-	slideVal = createP(slide.value());
-	slide.mouseMoved(changeSize);
+  cnv = createCanvas(800, 800);
+  rectMode(CENTER);
+  cnv.mouseClicked(makeBox);
+  createP("");
+  slide = createSlider(1, 50, 40, 1);
+  slideVal = createP(slide.value());
+  slide.mouseMoved(changeSize);
 
-	box.push(new Box(width/2, height/2, slide.value()));
+  box.push(new Box(width / 2, height / 2, slide.value()));
 }
 
 function draw() {
-	cnv.background(0);
+  cnv.background(0);
 
-	if (clicks === 0) {
-		fill(255);
-		textAlign(CENTER, CENTER);
-		textSize(72);
-		noStroke();
-		text("CLICK", width/2, height/2);
-	}
+  if (clicks === 0) {
+    fill(255);
+    textAlign(CENTER, CENTER);
+    textSize(72);
+    noStroke();
+    text("CLICK", width / 2, height / 2);
+  }
 
-	slideVal.html("Box Size: " + slide.value());
+  slideVal.html("Box Size: " + slide.value());
 
-	for (let b = box.length - 1; b > 0; b--) {
-		box[b].update();
-		box[b].show();
-	}
+  for (let b = box.length - 1; b > 0; b--) {
+    box[b].update();
+    box[b].show();
+  }
 }
 
 function makeBox() {
-	if (clicks === 0) {
-		clicks++;
-	}
-	if (box.length > 200) {
-		box.splice(0, 1);
-	} else {
-		box.push(new Box(width/2, height/2, slide.value()));
-	}
+  if (clicks === 0) {
+    clicks++;
+  }
+  if (box.length > 200) {
+    box.splice(0, 1);
+  } else {
+    box.push(new Box(width / 2, height / 2, slide.value()));
+  }
 }
 
 function changeSize() {
-	for (let b = box.length - 1; b > 0; b--) {
-		box[b].size = slide.value();
-	}
+  for (let b = box.length - 1; b > 0; b--) {
+    box[b].size = slide.value();
+  }
 }
